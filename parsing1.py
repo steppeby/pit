3b2b7eaab1dd260d66254b5767331375acb24fc2
steppeby <43454995+steppeby@users.noreply.github.com>
2023-05-19 22:18:22 +0500

    Update parsing1.py

--- parsing1.py
+++ parsing1.py
@@ -2,8 +2,8 @@ import requests
 from bs4 import BeautifulSoup as BS
 import pandas as pd
 
-url = "https://dinamarket.kz/ru/111/produkty/?lim=99999" #?lim=9999
-path = 'C:/Users/qwerty/Desktop/парс/my_data.xlsx'
+url = "https://dinamarket.kz/ru/111/produkty/?lim=9999" #?lim=9999 количество отображаемых продуктов выкручено до 10к для сбора с одной страницы
+export_path = 'C:/******/my_data.xlsx'
 res = requests.get(url)
 htmlData = res.content
 parsedData = BS(htmlData, "html.parser")
@@ -14,7 +14,7 @@ for i in a:
     res = i.find_all(attrs={"class":{"old","new","title"}})
     df = df.append({'Название': res[0].text.rstrip('\n').lstrip('\n'), 'Цена по скидке': res[1].text, 'Цена без скидки': res[2].text}, ignore_index=True)
 
-df.to_excel (path, index= False )
+df.to_excel (export_path, index= False )
 
 
 
